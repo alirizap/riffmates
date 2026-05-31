b4eaf22aad44951e796afc4213339d8e2814b2d8
Alireza Pishdar <pishdaralirza@gmail.com>
2026-05-31 20:08:44 +0330

    about view in home app

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def credits(request):
     content = "Nicky\nAlireza"
     return HttpResponse(content, content_type="text/plain")
 
+def about(request):
+    content = """
+        <h1>About Page</h1>
+        <p>A website for musicians!</p>
+    """
+    return HttpResponse(content, content_type="text/html")
