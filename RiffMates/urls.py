fd5511e50eee1c94865e4a6fd9757595b8ab284c
Alireza Pishdar <pishdaralirza@gmail.com>
2026-07-28 15:44:21 +0430

    feat: url for musician view and register bands urls

--- RiffMates/urls.py
+++ RiffMates/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from home import views as home_views
 
@@ -25,5 +25,6 @@ urlpatterns = [
     path('credits/', home_views.credits),
     path('about/', home_views.about),
     path('info/', home_views.info),
-    path('news/', home_views.news, name="news")
+    path('news/', home_views.news, name="news"),
+    path("bands/", include("bands.urls"))
 ]
