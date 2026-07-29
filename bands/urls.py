e7e0c9ed3897278cff7dde1a90af9c2e81cfac33
Alireza Pishdar <pishdaralirza@gmail.com>
2026-07-29 10:05:44 +0430

    feat: add bands list view

--- bands/urls.py
+++ bands/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from bands import views
 
 urlpatterns = [
+    path("", views.bands, name="bands"),
+    path("band/<int:band_id>", views.band, name="band"),
     path("musician/<int:musician_id>/", views.musician, name="musician"),
     path("musicians/", views.musicians, name="musicians"),
-    path("band/<int:band_id>", views.band, name="band"),
 ]
--- bands/views.py
+++ bands/views.py
@@ -48,3 +48,36 @@ def band(request, band_id):
     band = get_object_or_404(Band, id=band_id)
     data = {"band": band}
     return render(request, "band.html", data)
+
+
+def bands(request):
+    all_bands = Band.objects.all().order_by("name")
+
+    per_page = request.GET.get("per_page", 3)
+    try:
+        per_page = int(per_page)
+    except TypeError, ValueError:
+        per_page = 3
+
+    if per_page < 1:
+        per_page = 1
+    elif per_page > 20:
+        per_page = 20
+
+    paginator = Paginator(all_bands, per_page)
+
+    page_num = request.GET.get("page", 1)
+    try:
+        page_num = int(page_num)
+    except TypeError, ValueError:
+        raise Http404("Page must be a number")
+
+    if page_num < 1:
+        page_num = 1
+    elif page_num > paginator.num_pages:
+        page_num = paginator.num_pages
+
+    page = paginator.page(page_num)
+    data = {"bands": page.object_list, "page": page}
+
+    return render(request, "bands.html", data)
