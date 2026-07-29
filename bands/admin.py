0f89f82e91532821abef8207b476aa2542973297
Alireza Pishdar <pishdaralirza@gmail.com>
2026-07-29 11:09:07 +0430

    feat: add admin model for Musician

--- bands/admin.py
+++ bands/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from bands.models import Musician
 
-# Register your models here.
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = ("id", "last_name", "show_weekday")
+
+    def show_weekday(self, obj):
+        # Fetch weekday of artist's birth
+        return obj.birth.strftime("%A")
+
+    show_weekday.short_description = "Birth Weekday"
