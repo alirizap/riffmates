68de96b04eb4abce53f7d70d657551aeebadba0d
Alireza Pishdar <pishdaralirza@gmail.com>
2026-08-01 20:13:26 +0330

    feat: add url for restricted_page

--- bands/urls.py
+++ bands/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("band/<int:band_id>", views.band, name="band"),
     path("musician/<int:musician_id>/", views.musician, name="musician"),
     path("musicians/", views.musicians, name="musicians"),
+    path("restricted_page/", views.restricted_page, name="restricted_page"),
 ]
